36112cf88f9b51448a3bd79505262b4990ebb5e6
PolarState <4959869+PolarState@users.noreply.github.com>
2025-04-11 23:38:12 -0700

    Adding new dataset file generator.

--- cfg/cfg_datasets.py
+++ cfg/cfg_datasets.py
@@ -1,20 +1,50 @@
-import torch
+import os
+import struct
+from typing import Any
+
 from . import cfg_generator
 
-from typing import Any
+import torch
 
 
 class CFGFileDataset(torch.utils.data.Dataset):
+    """Dataset to load a cfg from a file.
+    
+    The file needs to already contain token ids.
+    
+    """
+
     def __init__(self, filename, device, window_length: int = 512):
         super().__init__()
         self.device = device
+        self.filename = filename
+        self.window_length = window_length
 
-        # load file.
+        self.file_size = os.stat(self.filename).st_size
+        self.dataset = []
+        self.attention_mask = [1] * self.window_length
+        self.idx = 0
+        self.length = self.file_size / self.window_length
 
-        # chop file into n segments.
+        with open(self.filename, "rb") as f:
+            while True:
+                bytes_to_read = self.window_length * struct.calcsize("i")
+                binary_chunk = f.read(bytes_to_read)
+                if not binary_chunk:
+                    break  # End of file
+
+                # Unpack the binary data into a tuple of integers
+                format_string = "!" + "i" * self.window_length
+                if len(binary_chunk) != struct.calcsize(format_string):
+                    raise ValueError(
+                        f"Unexpected end of file or corrupted data. Expected {struct.calcsize(format_string)} bytes, got {len(binary_chunk)}."
+                    )
+
+                token_list = list(struct.unpack(format_string, binary_chunk))
+                self.dataset.append(token_list)
 
     def __getitem__(self, index):
-        return torch.tensor(self.corpus[index], device=self.device)
+        return torch.tensor(self.dataset[index], device=self.device)
 
     def __len__(self):
         return self.length
@@ -39,9 +69,13 @@ class CFGRandomGenerationDataset(torch.utils.data.IterableDataset):
         self.window_length = window_length
         self.tokenizer = tokenizer
         self.device = device
+        self.eos_string = "E"
+        self.bos_string = "B"
+        self.eos_token = self.tokenizer.tokenize(self.eos_string)
+        self.bos_token = self.tokenizer.tokenize(self.bos_string)
 
         # Make the first token the Eos token as it's the divider token between datasets.
-        self.generation_buffer = self.tokenizer.encode(self.tokenizer.eos_token)
+        self.generation_buffer = []
 
     def __len__(self):
         return self.num_generations
@@ -52,26 +86,29 @@ class CFGRandomGenerationDataset(torch.utils.data.IterableDataset):
         return self
 
     def __next__(self):
-
         # Exit if we've completed all iterations.
         if self.idx >= len(self):
             raise StopIteration
 
         # Fill our generation buffer up to our widow length.
         while len(self.generation_buffer) < self.window_length:
+            self.generation_buffer.extend(self.bos_token)
             self.generation_buffer.extend(
-                self.tokenizer.encode(
-                    cfg_generator.generate_from_cfg(self.start_symbol, self.cfg_rules)
-                    + self.tokenizer.eos_token
+                self.tokenizer.tokenize(c)[0]
+                for c in cfg_generator.generate_from_cfg(
+                    self.start_symbol, self.cfg_rules
                 )
             )
+            self.generation_buffer.extend(self.eos_token)
 
         # Update our fake iterator length.
         self.idx += self.window_length
 
         # Generate tensors from our window.
         next_item = torch.tensor(
-            self.generation_buffer[: self.window_length],
+            self.tokenizer.convert_tokens_to_ids(
+                self.generation_buffer[: self.window_length]
+            ),
             device=self.device,
         )
 
---
+++ scripts/generate_dataset.py
@@ -0,0 +1,36 @@
+import os
+import struct
+import sys
+
+from cfg import cfg_datasets, cfg_defines, cfg_generator
+
+import transformers
+
+OUTPUT_FILE_PATH = "../datasets/test.bin"
+
+CFG = cfg_defines.cfg3b
+CONTEXT_LENGTH = 512
+
+cfg_start_symbols = list(cfg_generator.get_start_symbols(CFG))[0]
+
+tokenizer = transformers.GPTNeoXTokenizerFast.from_pretrained("openai-community/gpt2")
+
+new_dataset = cfg_datasets.CFGRandomGenerationDataset(
+    CFG,
+    cfg_start_symbols,
+    num_generations=1 * 96 * 512,
+    tokenizer=tokenizer,
+    window_length=CONTEXT_LENGTH,
+)
+
+with open(OUTPUT_FILE_PATH, "wb") as f:
+    for i, sample in enumerate(new_dataset):
+        token_list = sample.cpu().detach().numpy().tolist()
+        format_string = "!" + "i" * len(token_list)
+        binary_data = struct.pack(format_string, *token_list)
+
+        f.write(binary_data)
+        sys.stdout.write("\rDoing thing %i" % i)
+        sys.stdout.flush()
+
+print("COMPLETE")
